4d93872825b433d10323b73fefd0281be040437a
DiglanHimself <64547747+DiglanHMSLF@users.noreply.github.com>
2025-09-09 16:08:49 -0600

    Functioning Player Movement
    Still a weird issue with getting input that doesn't exist when the project starts...

--- MSUPalleteJam/Assets/Scripts/Player_Control.cs
+++ MSUPalleteJam/Assets/Scripts/Player_Control.cs
@@ -4,48 +4,50 @@ using UnityEngine.InputSystem;
 
 public class Player_Controller : MonoBehaviour
 {
-    private enum MovementState_e
-    {
-        IDLE,
-        GROUNDED,
-        AIRBORNE
-    }
-
+    //Someone might wanna clean up the Headers and SerializeFields later lmao (not it!!) - DC
 
     [Header("References")]
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private InputActionAsset _controls;
 
-
     [Header("Movement Settings")]
     [SerializeField] private float _jumpHeight = 2.0f;
     [SerializeField] private float _gravityScale = 1.0f;
-    [SerializeField] private float _moveSpeed = 2.0f;
-    [SerializeField] private LayerMask _ignore;
-
+    [SerializeField] private float _moveSpeed = 6.0f;
+    [SerializeField] private float _acceleration = 20.0f;
+    [SerializeField] private float _deceleration = 30.0f;
 
     [Header("Ground Check Settings")]
-    [SerializeField] private Vector2 _rectExtents = Vector2.one; 
+    [SerializeField] private Vector2 _rectExtents = Vector2.one;
+    [SerializeField] private Transform groundCast;
+    [SerializeField] private float rcDist = 0.1f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Header("Jump Assist")] // Right now, this only includes coyote time - DC
+    [SerializeField] private float coyoteTime = 0.1f; // seconds
 
     [Header("Debug")]
     [SerializeField] private Vector2 _movementDirection;
     [SerializeField] private bool _isJump;
-    [SerializeField] private bool _isAirborne;
+    [SerializeField] private bool _isGrounded;
     [SerializeField] private float _jumpVelocity;
-
 
     private InputAction _moveAction;
     private InputAction _jumpAction;
+    private bool _jumpQueued = false;
+    private float coyoteTimer = 0f;
 
     private void OnEnable()
     {
         _controls.FindActionMap("Player").Enable();
         _moveAction = _controls.FindActionMap("Player").FindAction("Move");
         _jumpAction = _controls.FindActionMap("Player").FindAction("Jump");
+        _jumpAction.started += OnJumpStarted;
     }
     private void OnDisable()
     {
         _controls.FindActionMap("Player").Disable();
+        _jumpAction.started -= OnJumpStarted;
     }
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -54,79 +56,104 @@ public class Player_Controller : MonoBehaviour
         _moveAction = _controls.FindActionMap("Player").FindAction("Move");
         _jumpAction = _controls.FindActionMap("Player").FindAction("Jump");
         _movementDirection = Vector2.zero;
-        _jumpVelocity = Mathf.Sqrt(2f * -Physics.gravity.y * _gravityScale * _jumpHeight);
 
+        _rb.gravityScale = 0f; // Disable built-in gravity
     }
 
     // Update is called once per frame
     void Update()
     {
         QueryPlayerInput();
-        CheckIsAirborne();
-    }
+        CheckGrounded();
 
+        // Update coyote timer
+        if (_isGrounded)
+            coyoteTimer = coyoteTime;
+        else
+            coyoteTimer -= Time.deltaTime;
+
+        // Sets Jump Velocity based on jump height and gravity scale. In update so that it can be changed in real-time for testing/mechanics. -DC
+        float gravity = -9.81f * _gravityScale;
+        _jumpVelocity = Mathf.Sqrt(2f * Mathf.Abs(gravity) * _jumpHeight);
+    }
 
     private void FixedUpdate()
     {
-
+        ApplyCustomGravity();
+        MovePlayer();
     }
 
-    private void QueryPlayerInput()
+    private void QueryPlayerInput() 
     {
-        // Get the input vector from the player, clamp the value so it can only be -1, 0, or 1, then feed the value back into the global direction vector
+        //John, I tweaked with the player input a bit to add a deadzone and log warnings for nonzero inputs cause we were getting some weird behavior
+        // regarding the player moving slightly on its own sometimes.
+        // It's totally still happening, but at least now we can see when it happens in the logs.
+        // Might wanna take a look at it if you have time. -DC
+
         Vector2 inputVector = _moveAction.ReadValue<Vector2>();
 
-        float vAxis = inputVector.y;
-        float hAxis = inputVector.x;
+        // Clamp small values to zero (deadzone)
+        if (Mathf.Abs(inputVector.x) < 0.2f)
+            inputVector.x = 0f;
+        if (Mathf.Abs(inputVector.y) < 0.2f)
+            inputVector.y = 0f;
 
-        vAxis = Mathf.Ceil(Mathf.Abs(vAxis)) * Mathf.Sign(vAxis);
-        hAxis = Mathf.Ceil(Mathf.Abs(hAxis)) * Mathf.Sign(hAxis);
+        // If the input is still not zero, log a warning for debugging
+        if (inputVector.x != 0f)
+            Debug.LogWarning($"Nonzero X input detected: {inputVector.x}");
 
-
-
-        _movementDirection = new Vector2(hAxis, vAxis);
-
-        _isJump = _jumpAction.IsPressed(); 
-        
+        _movementDirection = new Vector2(inputVector.x, 0f);
     }
 
+    private void OnJumpStarted(InputAction.CallbackContext ctx)
+    {
+        // Allow jump if grounded or within coyote time
+        if (_isGrounded || coyoteTimer > 0f)
+            _jumpQueued = true;
+    }
 
     private void MovePlayer()
     {
+        // Horizontal movement with acceleration/deceleration
+        float targetSpeed = _movementDirection.x * _moveSpeed;
+        float speedDiff = targetSpeed - _rb.linearVelocity.x;
+        float accelRate = (Mathf.Abs(targetSpeed) > 0.01f) ? _acceleration : _deceleration;
+        float movement = Mathf.Pow(Mathf.Abs(speedDiff) * accelRate, 0.9f) * Mathf.Sign(speedDiff);
 
+        _rb.AddForce(new Vector2(movement, 0f));
+
+        // Jumping
+        if (_jumpQueued && (coyoteTimer > 0f))
+        {
+            Vector2 v = _rb.linearVelocity;
+            v.y = _jumpVelocity;
+            _rb.linearVelocity = v;
+            _jumpQueued = false;
+            coyoteTimer = 0f; // Prevent double jump in coyote window
+        }
     }
 
-    // move the player using instantaneous velocity changes
-    private void MovePlayerGrounded()
+    private void CheckGrounded()
     {
-        Vector2 velocityVector = new Vector2(_movementDirection.x,0f);
-        
-        velocityVector = Vector3.Normalize(velocityVector) * _moveSpeed;
+        //Here's the BoxCast ground check, it works pretty well so far, but could totally be tweaked. -DC
 
-
-        float yComponent = 0f;
-
-        if (_isJump && !_isAirborne) yComponent = _jumpVelocity;
-
-
-        velocityVector = velocityVector + ((Vector2)transform.up * yComponent);
-
-        _rb.linearVelocity = velocityVector;
+        RaycastHit2D boxResult = Physics2D.BoxCast(gameObject.transform.position, new Vector2(2,1), 0f, Vector2.down, rcDist, groundMask);
+        _isGrounded = boxResult.collider != null;
     }
 
-
-    private bool IsInput()
+    private void ApplyCustomGravity() //Heres the shitty custom gravity function I made, it does pretty much what it should,
+                                      //but I know you wanted to use a real math function for it, so feel free to replace it. -DC
     {
-        return _movementDirection.x != 0.0f || _isJump; 
-    }
-
-    private void CheckIsAirborne()
-    {
-
+        if (!_isGrounded)
+        {
+            float gravity = -9.81f * _gravityScale;
+            _rb.AddForce(new Vector2(0, gravity), ForceMode2D.Force);
+        }
     }
 
     private void OnDrawGizmos()
     {
-        
+        if (groundCast != null)
+            Gizmos.DrawCube(groundCast.position + Vector3.down * rcDist, new Vector2(2, 1));
     }
 }
