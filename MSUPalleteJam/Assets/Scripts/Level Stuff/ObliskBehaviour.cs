a3e676d83f12f9e3586eaa8b4bdc75e1ae704382
John <johnjamisonfoth@gmail.com>
2025-09-14 06:12:05 -0600

    oblisk logic finished

--- MSUPalleteJam/Assets/Scripts/Level Stuff/ObliskBehaviour.cs
+++ MSUPalleteJam/Assets/Scripts/Level Stuff/ObliskBehaviour.cs
@@ -1,3 +1,6 @@
+using System.Collections;
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObliskBehaviour : MonoBehaviour
@@ -5,13 +8,60 @@ public class ObliskBehaviour : MonoBehaviour
     [SerializeField] private Animator _animator;
 
 
-    [SerializeField] private GameObject _projectile; 
+    [SerializeField] private GameObject _projectile;
+
+    [SerializeField] private Transform _projectileSpawnPoint;
+
+    [SerializeField] private float _shotCooldown = 1.0f;
+
+    [SerializeField] private float _shotTimeSeconds = 0.3f;
 
 
 
-    
 
-    
+    void Awake()
+    {
+        StartCoroutine(FireRoutine());
+    }
+
+
+    public IEnumerator FireRoutine()
+    {
+        yield return new WaitUntil(() => Player.Singleton != null);
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(_shotCooldown);
+
+            _animator.SetBool("IsFire", true);
+
+            yield return new WaitForSecondsRealtime(_shotTimeSeconds);
+
+            Vector2 playerPos = (Vector2)Player.Singleton.transform.position;
+
+            Vector2 projSpawnPos = (Vector2)_projectileSpawnPoint.position;
+
+            float dX = -(projSpawnPos.x - playerPos.x);
+            float dY = (projSpawnPos.y - playerPos.y);
+
+
+            float angle = Mathf.Atan2(dX, dY) * Mathf.Rad2Deg;
+
+
+            GameObject.Instantiate(_projectile, projSpawnPos, Quaternion.Euler(0f, 0f, angle));
+
+            _animator.SetBool("IsFire", false);
+
+        }
+
+
+
+
+    }
+
+
+
+
+
 
 
 }
--- MSUPalleteJam/Assets/Scripts/Level Stuff/ObliskLaser.cs
+++ MSUPalleteJam/Assets/Scripts/Level Stuff/ObliskLaser.cs
@@ -4,12 +4,21 @@ public class ObliskLaser : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D _rb;
     [SerializeField] private float _projectileSpeed;
+    [SerializeField] private float _lifeTime = 3f;
 
 
     void Awake()
     {
-        _rb.linearVelocity = -transform.up * _projectileSpeed; 
+        _rb.linearVelocity = -transform.up * _projectileSpeed;
 
+        Invoke(nameof(Cleanup), _lifeTime);
+
+    }
+
+
+    private void Cleanup()
+    {
+        Destroy(gameObject);
     }
 
 }
