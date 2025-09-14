196e7f92fa0df5eea3b40d4f1aa2d3f902aeb408
DiglanHimself <64547747+DiglanHMSLF@users.noreply.github.com>
2025-09-14 16:02:50 -0600

    Dash Thing

--- MSUPalleteJam/Assets/Scripts/Abilities/DashAbility.cs
+++ MSUPalleteJam/Assets/Scripts/Abilities/DashAbility.cs
@@ -3,11 +3,27 @@ using UnityEngine;
 public class DashAbility : Ability
 {
     [SerializeField] private AbilityData_t _abilityData;
+    [SerializeField] private bool _isCooldown;
+    [SerializeField] public ParticleSystem _dashEffect;
+    [SerializeField] private AbilityTracker _abilityTracker;
 
-    [SerializeField] private bool _isCooldown; 
+    private void Awake()
+    {
+        // Only assign if not set in the inspector
+        if (_dashEffect == null)
+        {
+            GameObject dashParticleObj = GameObject.Find("dashParticle");
+            if (dashParticleObj != null)
+            {
+                _dashEffect = dashParticleObj.GetComponent<ParticleSystem>();
+            }
+            else
+            {
+                Debug.LogWarning("DashAbility: No GameObject named 'dashParticle' found in the scene.");
+            }
+        }
+    }
 
-
-    
     public override void Cancel()
     {
         return;
@@ -17,14 +33,21 @@ public class DashAbility : Ability
     {
         if (_isCooldown) return; 
 
+        if (_dashEffect != null)
+        {
+            _dashEffect.Play();
+        }
+        else
+        {
+            Debug.LogWarning("DashAbility: _dashEffect is not assigned.");
+        }
+
         Player.Singleton.Controller.QueueDash();
         Debug.Log("Attempting to dash");
 
         _isCooldown = true;
         Invoke(nameof(EndCooldown), _abilityData.CooldownTimeSeconds);
     }
-
-
 
     private void EndCooldown()
     {
@@ -35,7 +58,6 @@ public class DashAbility : Ability
     {
         return _abilityData;
     }
-
 
     public override bool IsHoldAbility()
     {
