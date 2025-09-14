56f724d64b765c105372dd38499961256075153d
John <johnjamisonfoth@gmail.com>
2025-09-14 15:16:21 -0600

    Added Overworld Music To Start

--- MSUPalleteJam/Assets/Scripts/Level Stuff/MusicManager.cs
+++ MSUPalleteJam/Assets/Scripts/Level Stuff/MusicManager.cs
@@ -6,12 +6,19 @@ public class MusicManager : MonoBehaviour
     [SerializeField] private List<AudioClip> _tracks;
     [SerializeField] private AudioSource _source;
 
+    void Awake()
+    {
+        _source.clip = _tracks[0];
+        _source.Play();
+        _source.loop = true;
+    }
+
 
     public void StartNewTrack(int index)
     {
         _source.clip = _tracks[index];
         _source.Play();
-        _source.loop = true; 
+        _source.loop = true;
     }
 
     public void CancelCurrentTrack()
